5f7789f3b0dc1ac1132fe94766d97566d6f3b1ca
BotondProhaszka <73436429+BotondProhaszka@users.noreply.github.com>
2020-11-26 12:55:01 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,14 +2,16 @@ package application;
 
 import java.util.ArrayList;
 
+import application.Field.FieldState;
 import application.Snake.Type_Direction;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
@@ -17,6 +19,7 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
@@ -25,122 +28,154 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Game extends Thread{
-	protected Settings settings;
-	protected Map map;
-	protected ArrayList<Snake> snakeList;
-	protected ArrayList<Thread> threadList;
-	protected Scene menuScene;
+	private Settings settings;
+	private Map map;
+	private ArrayList<Snake> snakeList;
+	private ArrayList<Thread> threadList;
+	private Scene menuScene;
+	private ArrayList<MyText> pointList;
+	private MyText winnerMyText;
 	
 	
-	protected Game(Settings set) {
+	public Game(Settings set) {
 		settings = set;
 		map = new Map(set, this);
 		snakeList = new ArrayList<>();
 		threadList = new ArrayList<>();
+		pointList = new ArrayList<>();
 	}
 	
 	
-	protected Settings getSettings() {
+	public Settings getSettings() {
 		return settings;
 	}
 	
 	
-	protected void addSnakes() {
+	public void addSnakes() {
 		Snake s1 = new Snake(1, map);
 		snakeList.add(s1);
-		s1.placeSnake(map);
-		if(settings.getMultiplayer()) {
+		s1.placeSnake();
+		if(settings.getMode() == GameMode.multiplayer) {
 			Snake s2 = new Snake(2, map);
 			snakeList.add(s2);
-			s2.placeSnake(map);
+			s2.placeSnake();
+		} else if(settings.getMode() == GameMode.vsrobot) {
+			Snake s3 = new Snake(3, map);
+			snakeList.add(s3);
+			s3.placeSnake();
 		}
 	}
 	
-	protected void initGame(Stage stage) {
+	public void initGame(Stage stage) {
 		menuScene = stage.getScene();
 		initGameWindow(stage);
 		
 		
 	}
 	
-	protected void initGameWindow(Stage stage) {
+	public void initGameWindow(Stage stage) {
 		Pane game_map = new Pane();
-		int n = map.size;
-		
+		int n = map.getMapSize();
 		map.content(game_map);
 		
-		game_map.setMinHeight(n*map.sizeOfField);
-		game_map.setMinWidth(n*map.sizeOfField);
-        game_map.setPrefSize(n*map.sizeOfField, n*map.sizeOfField);
+		game_map.setMinHeight(n*map.getSizeOfField());
+		game_map.setMinWidth(n*map.getSizeOfField());
+        game_map.setPrefSize(n*map.getSizeOfField(), n*map.getSizeOfField());
         
         map.addFood();
         addSnakes();
 		
-		
-        GridPane points = new GridPane();
-        points.setPrefSize(n*map.sizeOfField, 80);
-        points.setStyle("-fx-background: RED");
-        points.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-        points.setLayoutX(0);
-        points.setAlignment(Pos.CENTER);
-        
         GridPane names = setName(stage);
-        //points.add(names, 1, 3);
-        Text t1 = snakeList.get(0).getSnakeName();
-        //Text p1 = snakeList.get(0).getSnake
-        if(settings.getMultiplayer()) {
-        	Text t2 = snakeList.get(1).getSnakeName();
-            points.add(t2, 3, 0);
-            t2.setFill(Color.BLACK);
-        }
-        points.setAlignment(Pos.CENTER);
-        GridPane.setMargin(t1, new Insets(10,100, 10,10));
+        GridPane points = initPoints(stage);
         
-        points.add(t1, 1, 0);
-
-        points.getNodeOrientation();
-        t1.setFill(Color.BLUE);
+        
+        
+        VBox winnerBox = new VBox();
+        winnerBox.setAlignment(Pos.CENTER);
+        
+        winnerMyText = new MyText("Who will win?", "large");
+        winnerMyText.setFill(Color.RED);
+        winnerBox.getChildren().add(winnerMyText);
         
         GridPane gp = new GridPane();
-        gp.setHgap(10);
-        gp.setVgap(10);
-        gp.setPadding(new Insets(10, 10, 10, 10));
-        
-        
-        gp.setMinHeight(n*map.sizeOfField);
-        gp.setMinWidth(n*map.sizeOfField);
-        gp.setStyle("-fx-background: LIME");
-        gp.add(points, 0, 1);
-        gp.add(game_map, 0, 3);
-        gp.add(names, 0, 2);
+        gp.setHgap(20);
+        gp.setVgap(20);
         gp.setAlignment(Pos.CENTER);
         
         
-        addButtons(stage, gp, names);
+        gp.setMinHeight(n*map.getSizeOfField());
+        gp.setMinWidth(n*map.getSizeOfField());
+        gp.setStyle("-fx-background: LIME");
+        gp.add(names, 0, 1);
+        gp.add(points, 0, 2);
+        gp.add(winnerBox, 0, 3);
+        gp.add(game_map, 0, 4);
+        gp.add(createButtons(stage, gp, names), 0, 5);
+        gp.setAlignment(Pos.CENTER);
+        
+        
+        
         
 
         Scene s = new Scene(gp);
         
         handleActions(s);
-        stage.setMinHeight(n*map.sizeOfField + 200);
-        stage.setMinWidth(n*map.sizeOfField + 200);
+        stage.setMinHeight(n*map.getSizeOfField() + 400);
+        stage.setMinWidth(n*map.getSizeOfField() + 200);
         stage.setScene(s);
         stage.show();
 	}
 	
-	protected void countDown(Stage s) {
-		Pane p = new Pane();
-		Text t = new Text("3");
-		p.getChildren().add(t);
-		Scene sc = new Scene(p);
-		s.setScene(sc);
+	public GridPane initPoints(Stage stage) {
+		GridPane points = new GridPane();
+        points.setPrefSize(map.getMapSize()*map.getSizeOfField(), 80);
+        points.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+        points.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+        points.setAlignment(Pos.CENTER);
+        
+        
+        
+        MyText name1 = snakeList.get(0).getSnakeNameMyText();
+        name1.setFill(map.getMapElement(0).getSnake1Color());
+        MyText point1 = snakeList.get(0).getPointMyText();
+        point1.setFill(map.getMapElement(0).getSnake1Color());
+        
+        pointList.add(point1);
+        if(settings.getMode() == GameMode.multiplayer) {
+        	MyText name2 = snakeList.get(1).getSnakeNameMyText();
+        	name2.setFill(map.getMapElement(0).getSnake2Color());
+        	MyText point2 = snakeList.get(1).getPointMyText();
+        	point2.setFill(map.getMapElement(0).getSnake2Color());
+        	pointList.add(point2);
+        	points.add(name2, 1, 2);
+        	points.add(point2, 2, 2);
+        } else if(settings.getMode() == GameMode.vsrobot) {
+        	MyText name2 = snakeList.get(1).getSnakeNameMyText();
+        	name2.setFill(map.getMapElement(0).getSnake2Color());
+        	MyText point2 = snakeList.get(1).getPointMyText();
+        	point2.setFill(map.getMapElement(0).getSnake2Color());
+        	pointList.add(point2);
+        	points.add(name2, 1, 2);
+        	points.add(point2, 2, 2);
+        }
+        points.setAlignment(Pos.CENTER);
+        GridPane.setMargin(name1, new Insets(10,100, 10,10));
+        
+        points.add(name1, 1, 1);
+        points.add(point1, 2, 1);
+        points.getNodeOrientation();
+        return points;
 	}
 	
-	protected GridPane setName(Stage s) {
-		GridPane gp = new GridPane();
 	
+	public GridPane setName(Stage s) {
+		GridPane gp = new GridPane();
+		
+		gp.setVgap(10);
+		gp.setHgap(10);
+		
 		gp.setAlignment(Pos.CENTER);
-		gp.setPrefSize(map.size*map.sizeOfField, 80);
+		gp.setPrefSize(map.getMapSize()*map.getMap().get(0).getSize(), 80);
         gp.setStyle("-fx-background: LIME");
         gp.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
         
@@ -151,165 +186,119 @@ public class Game extends Thread{
         gp.add(t1, 1, 1);
         gp.add(tf1, 2, 1);
         TextField tf2 = new TextField();
-        if(settings.getMultiplayer()) {
+        if(settings.getMode() == GameMode.multiplayer) {
 	        Text t2 = new Text("Second player: ");
 	        t2.setFont(Font.font("impact", FontWeight.EXTRA_LIGHT, FontPosture.REGULAR, 10));
 			t2.setFill(Color.BLACK);
 	        
 	        gp.add(t2, 1, 2);
 	        gp.add(tf2, 2, 2);
-        }
+        }         
         
-        
-        Button button = new Button("Okay");
-        button.setMinSize(50, 30);
-        button.setPrefSize(50, 30);
+        MyButton button = new MyButton("Okay", "small");
         gp.add(button, 3, 1);
         
         
-        
-        button.setStyle("-fx-background-color: BLACK;"
-        		+ "-fx-font-weight: BOLD;"
-        		+ "-fx-font-size: 12;"
-        		+ "-fx-text-fill: LIME");
-        
         button.setOnAction(e -> {
         	snakeList.get(0).setSnakeName(tf1.getText());
-        	if(settings.getMultiplayer())
+        	if(settings.getMode() == GameMode.multiplayer)
         		snakeList.get(1).setSnakeName(tf2.getText());
-        	
+        	else if(settings.getMode() == GameMode.vsrobot)
+        		snakeList.get(1).setSnakeName("Robot");
         });
         
        return gp;     
 	}
 	
-	protected void stopGame() {
-		
-		settings.setGameRuns(false);
-		threadList.forEach((n) -> n.stop());
-	}
-	
-	protected void startGame() {
-		settings.setGameRuns(true);
+	public void startGame() {
 		Thread th1 = new Thread(snakeList.get(0));
 		threadList.add(th1);
-		if(settings.getMultiplayer()) {
+		if(settings.getMode() != GameMode.singleplayer) {
 			Thread th2 = new Thread(snakeList.get(1));
 			threadList.add(th2);
 			th2.start();
 		}
 		th1.start();
-		System.out.println("start");
 	}
 	
-	protected void addButtons(Stage stage, GridPane gp, GridPane names) {
+	public void stopGame() {
+		
+		settings.setGameRuns(false);
+		threadList.forEach((n) -> n.stop());
+		boolean en = false;
+		for(Snake s : snakeList) {
+			s.getErremenj().clear();
+			if(s.getPoints() != 0)
+				en = true;
+		}
+		if(en)
+			settings.getScoreboard().addPoints(snakeList);
+	}
+	
+	public GridPane createButtons(Stage stage, GridPane parent, GridPane names) {
+		GridPane gp = new GridPane();
+		gp.setAlignment(Pos.CENTER);
 		GridPane buttons = new GridPane();
-	       
-	    Button startButton = new Button("START");
+		buttons.setVgap(10);
+		buttons.setHgap(10);
+		
+		
+	    MyButton startButton = new MyButton("START", "medium");
 	    buttons.setAlignment(Pos.CENTER);
 	    buttons.add(startButton, 0, 0);
-	    startButton.setMinSize(60, 40);
-	    startButton.setPrefSize(60, 40);
-	        
-	        
-	        
-	    startButton.setStyle("-fx-background-color: BLACK;"
-	    		+ "-fx-font-weight: BOLD;"
-	        	+ "-fx-font-size: 12;"
-	        	+ "-fx-text-fill: LIME");
+	    
 	        
 	    startButton.setOnAction(e -> {
-	    	gp.getChildren().remove(names);
-	        startGame();
+	    	parent.getChildren().remove(names);
+	        if(settings.getGameRuns())
+	        	startGame();
 	    });
 	    
-	    Button newButton = new Button("NEW");
+	    MyButton newButton = new MyButton("NEW", "medium");
 	    buttons.setAlignment(Pos.CENTER);
 	    buttons.add(newButton, 1, 0);
-	    newButton.setMinSize(60, 40);
-	    newButton.setPrefSize(60, 40);	        
-	        
-	        
-	    newButton.setStyle("-fx-background-color: BLACK;"
-	    		+ "-fx-font-weight: BOLD;"
-	        	+ "-fx-font-size: 12;"
-	        	+ "-fx-text-fill: LIME");
-	        
+	    
 	    newButton.setOnAction(e -> {
 	    	stopGame();
-	    	snakeList.forEach((n) -> n.removeSnake(map));
-	    	String name1 = snakeList.get(0).getName();
-	    	String name2 = new String();
-	    	if(settings.getMultiplayer())
-	    		name2 = snakeList.get(0).getName();
-	    	
-	    	snakeList.clear();
-	    	addSnakes();
-	    	snakeList.get(0).setName(name1);
-	    	if(settings.getMultiplayer())
-	    		snakeList.get(1).setName(name2);
+	    	map.getMap().forEach((n) -> n.setFieldState(FieldState.empty));
+	    	for(int i = 0; i < snakeList.size(); i++) {
+	    		snakeList.get(i).resetSnake();
+	    	}
+	    	//snakeList.forEach((n) -> n.resetSnake());
 	    	map.addFood();
-	    	setName(stage);
+	    	winnerMyText.setText("Who will win?");
+	    	settings.setGameRuns(true);
 	    });
 	    
-	    /*
-	    Button stepButton = new Button("MOVE");
+	    MyButton exitButton = new MyButton("EXIT", "medium");
+	    settings.setGameRuns(true);
 	    buttons.setAlignment(Pos.CENTER);
-	    buttons.add(stepButton, 3, 0);
-	    stepButton.setMinSize(60, 40);
-	    stepButton.setPrefSize(60, 40);	        
-	        
-	        
-	    stepButton.setStyle("-fx-background-color: BLACK;"
-	    		+ "-fx-font-weight: BOLD;"
-	        	+ "-fx-font-size: 12;"
-	        	+ "-fx-text-fill: LIME");
-	    
-	    stepButton.setOnAction(e -> {
-	    	snakeList.get(0).move();
-	    });
-	    
-	    
-	    Button updButton = new Button("UPDATE");
-	    buttons.setAlignment(Pos.CENTER);
-	    buttons.add(updButton, 4, 0);
-	    updButton.setMinSize(60, 40);
-	    updButton.setPrefSize(60, 40);	        
-	        
-	        
-	    updButton.setStyle("-fx-background-color: BLACK;"
-	    		+ "-fx-font-weight: BOLD;"
-	        	+ "-fx-font-size: 12;"
-	        	+ "-fx-text-fill: LIME");
-	    
-	    updButton.setOnAction(e -> {
-	    	t1.setText(snakeList.get(0).getMoveText()); 	
-	    	t2.setText(snakeList.get(0).getLastText());
-	    });
-	    
-	    */
-	    Button exitButton = new Button("EXIT");
-	    buttons.setAlignment(Pos.CENTER);
-	    buttons.add(exitButton, 3, 0);
-	    exitButton.setMinSize(60, 40);
-	    exitButton.setPrefSize(60, 40);	        
-	        
-	        
-	    exitButton.setStyle("-fx-background-color: BLACK;"
-	    		+ "-fx-font-weight: BOLD;"
-	        	+ "-fx-font-size: 12;"
-	        	+ "-fx-text-fill: LIME");
+	    buttons.add(exitButton, 2, 0);
 	    
 	    exitButton.setOnAction(e -> {
+	    	stopGame();
 	    	stage.setScene(menuScene);
 	    });
 	    
 	    gp.add(buttons, 0, 4);
-
+	    return gp;
 	}
 	
+	public void snake1Loses() {
+		if(snakeList.size() == 1)
+			winnerMyText.setText(":(");
+		else
+			winnerMyText.setText(snakeList.get(1).getSnakeName() + " won!");
+		
+	}
+	public void snake2Loses() {
+		winnerMyText.setText(snakeList.get(0).getSnakeName() + " won!");
+	}
+	public void draw() {
+		winnerMyText.setText("DRAW!");
+	}
 	
-	protected void handleActions(Scene scene) {
+	public void handleActions(Scene scene) {
 		scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
 			if(settings.getGameRuns()) {
 				if(key.getCode() == KeyCode.W) {
@@ -321,14 +310,14 @@ public class Game extends Thread{
 				} else if(key.getCode() == KeyCode.D) {
 					snakeList.get(0).setMoveDir(Type_Direction.right);
 				} 
-				if(settings.getMultiplayer()) {
-					if(key.getCode() == KeyCode.I) {
+				if(settings.getMode() == GameMode.multiplayer) {
+					if(key.getCode() == KeyCode.UP) {
 						snakeList.get(1).setMoveDir(Type_Direction.up);
-					} else if(key.getCode() == KeyCode.J) {
+					} else if(key.getCode() == KeyCode.LEFT) {
 						snakeList.get(1).setMoveDir(Type_Direction.left);
-					} else if(key.getCode() == KeyCode.K) {
+					} else if(key.getCode() == KeyCode.DOWN) {
 						snakeList.get(1).setMoveDir(Type_Direction.down);
-					} else if(key.getCode() == KeyCode.L) {
+					} else if(key.getCode() == KeyCode.RIGHT) {
 						snakeList.get(1).setMoveDir(Type_Direction.right);
 					}
 				}
