3c97eaf59c75d77003a63d84e1991d083c3b0524
BotondProhaszka <73436429+BotondProhaszka@users.noreply.github.com>
2020-11-26 12:53:37 +0100

    Update Field.java

--- Field.java
+++ Field.java
@@ -8,42 +8,53 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Field extends StackPane {
-	protected enum FieldState {empty, snake1_body, snake1_head, snake2_body, snake2_head, food};
+	public enum FieldState {empty, snake1_body, snake1_head, snake2_body, snake2_head, food};
 	
 	Rectangle border;
-	protected FieldState state;
-	//protected Image img;
+	private FieldState state;
 	
-	//private int id = 0;
 	
 	private Color empty_color = Color.BLACK;
-	private Color snake1_color = Color.DEEPPINK;
-	private Color snake2_color = Color.BLUE;
+	private Color snake1_color = Color.BLUE;
+	private Color snake2_color = Color.YELLOW;
 	private Color food_color = Color.RED;
+	private int size;
 	
-	
-	protected Field(FieldState s) {
-		state = s;
-		border = new Rectangle(9, 9);
-		
-		//img = new Image("file:grass.jpg", 9, 9, true, true);
-        //ImageView iv = new ImageView(img);
-        
-		setBorderFill(s);
-		//border.setStroke(Color.BLACK);
+	public Field(FieldState fs, int s) {
+		size = s;
+		state = fs;
+		border = new Rectangle(size, size);
+		setBorderFill(fs);
 		getChildren().addAll(border);
 	}
 	
-	protected synchronized FieldState getFieldState() {
+	public synchronized FieldState getFieldState() {
 		return state;
 	}
 	
-	protected synchronized void setFieldState(FieldState fs) {
+	public Color getEmptyColor() {
+		return empty_color;
+	}
+	public Color getSnake1Color() {
+		return snake1_color;
+	}
+	public Color getSnake2Color() {
+		return snake2_color;
+	}
+	public Color getFoodColor() {
+		return food_color;
+	}
+	public int getSize() {
+		return size;
+	}
+	
+	
+	public synchronized void setFieldState(FieldState fs) {
 		state = fs;
 		setBorderFill(fs);
 	}
 	
-	protected synchronized void setBorderFill(FieldState s) {
+	public synchronized void setBorderFill(FieldState s) {
 		if(s == FieldState.empty) {
 			//border.setFill(new ImagePattern(img));
 			border.setFill(empty_color);
