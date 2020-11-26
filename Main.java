3b33b946cb1319b962136d6ac03f485e784e4703
BotondProhaszka <73436429+BotondProhaszka@users.noreply.github.com>
2020-11-26 12:55:22 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
 package application;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		       @Override
+		       public void handle(WindowEvent e) {
+		          Platform.exit();
+		          System.exit(0);
+		       }
+		 });
 		 MyInterface myif = new MyInterface(primaryStage);
 		 myif.globalInit();
 
